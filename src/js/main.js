65183bfba15add882c37e44cbce6fe84f23b5e3b
Valery Mashenets <webvalery.git@gmail.com>
2022-02-19 23:05:19 +0300

    sum

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,30 @@
-let sum = (a,b) => {
-  return a + b;
+let exchangeEuro = 1.13;
+let exchangeGbp = 1.36;
+let exchangeJpy = 0.0087;
+let exchangeRub = 0.013;
+
+// Событие клик на кнопку Start
+let btnStart = document.querySelector('.start__form-btn');
+btnStart.addEventListener('click', check);
+
+
+function check(event) {
+  event.preventDefault();
+  let inputDollar =+ document.querySelector('.start__currency--dollar').value;
+  let inputEuro =+ Math.ceil(document.querySelector('.start__currency--euro').value * exchangeEuro);
+  let inputGbp =+ Math.ceil(document.querySelector('.start__currency--gbp').value * exchangeGbp);
+  let inputJpy =+ Math.ceil(document.querySelector('.start__currency--jpy').value * exchangeJpy);
+  let inputRub =+ Math.ceil(document.querySelector('.start__currency--rub').value * exchangeRub);
+  
+  let checkSum = document.querySelector('.sum__button').checked; 
+  if(checkSum === true) {
+    let sumInput = inputDollar + inputEuro + inputGbp + inputJpy + inputRub;
+    document.querySelector('.top__result').textContent = `Привет, Димон. В твоём кошельке все вместе: ${sumInput}$`
+  } else {
+    console.log('Выключено');
+  }
 }
-console.log(sum(3,30));
+
+// fetch('http://api.coinlayer.com/live?access_key=4ed147814b987e83ed288fd73aa3e14f')
+//   .then(response => response.json())
+//   .then(resp => console.log(resp));
