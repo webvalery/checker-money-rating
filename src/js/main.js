4ab29c608e03e9d8b51b1550e48aede56fbfdad4
Valery Mashenets <webvalery.git@gmail.com>
2022-02-21 18:19:39 +0300

    upd hide sum

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,24 @@ let exchangeGbp = 1.36;
 let exchangeJpy = 0.0087;
 let exchangeRub = 0.013;
 
+// Скрытие суммы 
+let checkboxScreen = document.querySelector('.sum__button');
+let hideScreen = document.querySelector('.start__form-btn-hide');
+checkboxScreen.addEventListener('change', () => {
+  if (checkboxScreen.checked) {
+    hideScreen.classList.add('start__form-btn-hide--active');
+    document.querySelectorAll('.start__currency').forEach(element => {
+      element.setAttribute('type', 'password');
+    });    
+  } else {
+    hideScreen.classList.remove('start__form-btn-hide--active');
+    document.querySelectorAll('.start__currency').forEach(element => {
+      element.setAttribute('type', 'text');
+    });    
+  }
+})
+
+
 // Событие клик на кнопку Start
 let btnStart = document.querySelector('.start__form-btn');
 btnStart.addEventListener('click', checkData);
@@ -17,24 +35,24 @@ function checkData(event) {
   let inputRub =+ document.querySelector('.start__currency--rub').value * exchangeRub;
 
   let sumInput = inputDollar + inputEuro + inputGbp + inputJpy + inputRub;
-  renderResoult(sumInput);
+  calcResoult(sumInput);
 }
 
-function renderResoult(sumInput) {
+function calcResoult(sumInput) {
   let topRes;
   let step;
   for(let i = 0; i < ranges.length; i++) {
     if(sumInput >= ranges[i].upper) {
       step = (ranges[i-1].upper - ranges[i-1].lower) / ranges[i-1].amount; // вычисляем шаг в диапазоне
       topRes = Math.floor(ranges[i-1].topStart + (ranges[i-1].upper - sumInput) / step);
-      document.querySelector('.top__result').textContent = `${topRes}`;
       break;
     } else if (sumInput < ranges[ranges.length - 1].upper) {
       step = (ranges[ranges.length-1].upper - ranges[ranges.length-1].lower) / ranges[ranges.length-1].amount;
       topRes = Math.floor(ranges[ranges.length-1].topStart + (ranges[ranges.length-1].upper - sumInput) / step);
-      document.querySelector('.top__result').textContent = `${topRes}`;
       break;
     }
   }
+  document.querySelector('.top__result').textContent = `${topRes.toLocaleString('ru')}`;
 }
+
 
